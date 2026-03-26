22bcb2ad1fc03240c02939bc255fe1f3642f2adb
GavrilS <gavr1lll@abv.bg>
2026-03-26 07:30:40 +0200

    Adding a script to setup the app database docker container

---
+++ database/setup_mariadb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Setting up mariadb with docker
+# Pulling the image
+echo "To look for a specific MariaDB image run the command:"
+echo "  docker search mariadb"
+echo "Pick a version and then run:"
+echo "  docker pull mariadb:<version>"
+echo "Or to get the latest just run:"
+echo "  docker pull mariadb"
+read -p "Do you want to pull the latest version? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    echo "Pulling the latest MariaDB image"
+    sudo docker pull mariadb
+else
+    echo "Skipping image pull..."
+fi
+echo "************"
+echo ""
+
+# Running a container
+echo "To run a container use the command:"
+echo "  docker run --name finance_app -e MYSQL_ROOT_PASSWORD=mariadb -p 3308:3308 -d mariadb"
+# - name - the name for the container
+# - e - specifies the password for the root user
+# - p - sets a mapping of the port of the container and the host
+# - d - run in detach mode
+# - at the end we specify the image to be used
+read -p "Do you want to run the container with the default arguments from the command above? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    echo "Running the container with the default arguments"
+    sudo docker run --name finance_app -e MYSQL_ROOT_PASSWORD=mariadb -p 3308:3308 -d mariadb
+else
+    echo "Skipping running the container..."
+fi
+echo "************"
+echo ""
+
+# Connect to MariaDB and execute database setup sql script
+read -p "Do you want to configure the App database? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    echo "Connecting to the app database"
+    mariadbIP="$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' finance_app)"
+    echo "The assigned IP for the container is - $mariadbIP"
+    sudo mysql -h $mariadbIP -u root -p < setup_db_finance_app.sql
+else
+    echo "Skipping the App DB configuration at the moment!"
+fi
